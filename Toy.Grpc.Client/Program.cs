71447326b74aa50f4999858c86f66d34144ef817
rd-dhyoo <prtrans23@gmail.com>
2021-08-06 00:41:33 +0900

    초기 커밋.

---
+++ Toy.Grpc.Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Toy.Grpc.Client
+{
+    // https://docs.microsoft.com/ko-kr/aspnet/core/tutorials/grpc/grpc-start?view=aspnetcore-5.0&tabs=visual-studio
+
+    // before to do
+    //Install-Package Grpc.Net.Client
+    //Install-Package Google.Protobuf
+    //Install-Package Grpc.Tools
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start");
+            Console.ReadKey();
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
